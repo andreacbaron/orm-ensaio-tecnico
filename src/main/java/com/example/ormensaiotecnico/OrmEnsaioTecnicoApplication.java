be0f9032c166b0ab18cd5785c3559fc601762b87
Andrea Caitano Baron <andrea.caitano.baron@avanade.com>
2023-09-08 23:15:46 -0300

    estrutura inicial

---
+++ src/main/java/com/example/ormensaiotecnico/OrmEnsaioTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ormensaiotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmEnsaioTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmEnsaioTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ormensaiotecnico/OrmEnsaioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ormensaiotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmEnsaioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
